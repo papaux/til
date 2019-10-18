3c98d0d06d01b58df6abb77d9b8918b4af3552fd
Geoffrey Papaux <geoffrey.papaux@gmail.com>
2019-10-18 22:30:35 +0200

    build: add script for generating github pages output

---
+++ build-gh-pages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##
+# Build into html file format for github pages.
+# This assumes you have a folder ../til-gh-pages which tracks the gh-pages branch
+##
+
+GH_PAGES=../til-gh-pages
+
+if [ ! -d "$GH_PAGES" ]; then
+    echo "ERROR: Cannot proceed becaus the target directory $GH_PAGES does not exist."
+    exit 1
+fi
+
+make html -e BUILDDIR=$GH_PAGES
+
